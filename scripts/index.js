2ec917fb7efbae91716148e7f99632a28c6617a0
Camille Demichel <camilledemichel@MacBook-Air-de-Camille-2.local>
2015-08-12 11:29:28 +0800

    onàinàin

--- scripts/index.js
+++ scripts/index.js
@@ -12,12 +12,27 @@ var dataset2 = [];
 //tableau de données à importer
 var dataset = [];
 
-d3.csv("scripts/qd.csv")
-    .row(function(d) {
-        if(isNaN(bac(d.bacplus)))return;
-        return {naissance : +d.naissance, bacplus:bac(d.bacplus), pays: d.pays,
-            statut : d.statut,formation : d.formation,inscription : d.inscription,finance : d.finance,
-            uid:d.naissance+"|"+d.bacplus};
+//d.sexe=Math.floor(Math.random()*2);
+
+//d3.csv("scripts/qd.csv")
+d3.csv("scripts/questionnaire.csv")
+    .row(function(d, i) {
+        //console.log(d);
+        console.log(d['Quelle est votre année de naissance ?']);
+        if(isNaN(bac(d["Quel est votre niveau de formation académique ?"])))return;
+        if(d['Quelle est votre année de naissance ?']<1895)return;
+        var obj={
+            naissance: d['Quelle est votre année de naissance ?'],
+            sexe:Math.floor(Math.random()*3),
+            bacplus:bac(d["Quel est votre niveau de formation académique ?"]),
+            pays: d['Quel est votre pays de résidence ?'],
+            statut : d["Quel est votre statut professionnel ?"],
+            formation : d['Quelle est votre formation ?'],
+            inscription : d['Etes-vous inscrit à titre personnel ou par votre entreprise ?'],
+            finance : d['Exercez-vous un métier directement dans la finance ?'],
+            uid:d['Quelle est votre année de naissance ?']+"|"+d["Quel est votre niveau de formation académique ?"]
+        };
+        return obj;
     })
     .get(function(error, rows) {
         //console.log(rows);
@@ -31,59 +46,20 @@ d3.csv("scripts/qd.csv")
             d.bacplus=bac(d.key.split('|')[1]);
             d.n=d.values.length;
         });
+        affichePays();
+        d3.selectAll('input.checkbox').attr('checked','true');
+        $('input').change(function(){update2()});
 
         update2();
     });
 
 
-d3.selectAll('input.checkbox').attr('checked','true');
 
 //var color=d3.color();
 var color=d3.scale.category10();
 
 
 
-/*
-
- function addNumber(toAdd) {
- this.nombre = toAdd;
- }
-
- /* var count = function (d){
- for (var )
- }
-
- update();
- });
-
-
- var comparer = function (a,b) {
- if (a.naissance === b.naissance && a.bacplus === b.bacplus )
- {
- return 1;
- }
- else
- {
- return 0;
- }
- }
-
- var compter = function (a) {
- var nombre=0
- for(var b=0;b=50;b++)
- {
- nombre = nombre + comparer(a,dataset[b]);
- }
- return nombre;
- }
-
-
-
- console.log(compter(dataset[1]));
-
-
- */
-// Compteur brutal.
 
 
 console.log('dataset',dataset);
@@ -103,7 +79,7 @@ function removeone(){
 
 //hauteur et largeur du scatter plot à revoir
 var h=300;
-var l=$('.container').width()-220;
+var l=$('.container').width();
 svg.attr("width",l);
 svg.attr("height",h);
 //marge aux extrémités
@@ -111,104 +87,61 @@ var m = 80;
 
 
 
-// ---------------------------------------------------UPDATE------------------------------------------------------------
-/*
-function update()
-{
+
+//nombre en couleur à côté des pays
 
 
-    //                                   ----------échelles------------
-    d3.selectAll(".axis").remove();
-    var xScale = d3.scale.linear()
-        .domain([d3.min(dataset2,function(d){return d.naissance;}),d3.max(dataset2,function(d){return d.naissance})])
-        .range([m,l-m]);
+function listePays(a) {
+    var counts = {};
+    a.forEach(function (d) {
+        if (!counts[d.pays]) {
+            counts[d.pays] = 0;
+        }
+        counts[d.pays]++;
+    });
+    return counts;
+};
 
-    var max=d3.max(dataset2,function(d){return d.bacplus;});
+function affichePays() {
+    var listedePays = listePays(dataset);
+    console.log(listedePays);
+    var blocHTML = "";
 
-    console.log(max);
-    var yScale = d3.scale.linear()
-        .domain([d3.min(dataset2,function(d){return d.bacplus;}),d3.max(dataset2,function(d){return d.bacplus;})]) //à transformer en [0,7] ?
-        .range([h-m,m]);
+   for (var k in listedePays) {
+       console.log(k,listedePays[k]);
+       blocHTML = blocHTML + "<label for=\"" + k + "\" style='font-weight: normal'><input type=checkbox class=checkbox id=\"" + k + "\"> &nbsp " + k + " <span style='color: " + color(k) + "'><strong> " + listedePays[k] + " </strong></span></label><br>";
+   };
 
-
-    //                                      ----------axe et lignes--------
-    var xAxis = d3.svg.axis()
-        .scale(xScale)
-        .orient("bottom")
-        .tickFormat(d3.format("04d"));
-
-    svg.append("g").attr("class","axis")
-        .attr("transform", "translate(0," + (h-20) + ")")
-        .text("Année de naissance")
-        .call(xAxis);
+    $("#pays").html(blocHTML);
+};
 
 
 
+function listeFinance(a) {
+    var counts = {};
+    a.forEach(function (d) {
+        if (!counts[d.finance]) {
+            counts[d.finance] = 0;
+        }
+        counts[d.finance]++;
+    });
+    return counts;
+};
+
+function afficheFinance() {
+    var listedeFinance = listeFinance(dataset);
+    console.log(listedeFinance);
+    var blocHTML = "";
+
+    for (var f in listedeFinance) {
+        console.log(f,listedeFinance[f]);
+        blocHTML = blocHTML + "<p><label for=" + f + " style='font-weight: normal'><input type=checkbox class=checkbox id=" + f + "> &nbsp " + f + " <span style='color: " + color(f) + "'><strong> " + listedeFinance[f] + " </strong></span></label></p>";
+    };
+
+    $("#finance").html(blocHTML);
+};
 
 
-    //Lignes
-    var dat=[2,3,4,5];
-    //dat=d3.range(2,6);
-    console.log("ello",dat);
-    var lines=svg.selectAll("line.h").data(dat);
-    lines.enter()
-        .append("line")
-        .attr("class",'h')
-        .attr("x1",m-10)
-        .attr("y1",function (d) { return yScale(d);})
-        .attr("x2",l-m+10)
-        .attr("y2",function (d) { return yScale(d);})
-        .attr("stroke-width",0.5)
-        .attr("stroke","#eee");
-
-
-
-    //----------------------cercles-----------------------
-
-    var circles = svg.selectAll("circle").data(dataset2);
-
-    circles.enter()
-        .append("circle")
-        .attr('stroke', 'teal')
-        .attr('stroke-width', 0)
-        .attr("cx", function (d) {
-            var abs = (xScale(d.naissance));
-            return abs;})
-        .attr("cy", function (d,i) {
-            var ord = (yScale(d.bacplus));
-            return ord;
-        })
-        .attr("r", 3)
-        .attr("class", "cercle")
-
-        .on('mouseover', function (d,i) {
-            //var a = xScale(d[0]);
-            //var b = yScale(d[1]);
-            console.log(d);
-            d3.select(this).attr('stroke-width', 3)
-            //ci-dessous : étiquette
-        })
-        .on('mouseout', function (d) {
-            d3.select(this).attr('stroke-width', 0);
-        })
-        .append("svg:title")
-        .text(function (d, i) {
-            return "Nombre d'adhérents : " + 1 + "\nAnnée de naissance : " + d.naissance + "\nNiveau d'études : bac+" + d.bacplus;
-        })
-    ;
-}
-*/
-
-/*circles.transition(100)
- .attr("r",function(d){return zScale(1);});
-
-
-
- circles.exit()
- .attr("r",function(d){return 0;})
- .remove();
-
- */
 
 
 
@@ -218,6 +151,9 @@ function update2() {
     console.log('update2()');
 
     d3.selectAll(".axis").remove();
+
+    l = $('.container').width();
+    svg.attr("width", l);
 
     //Échelles
     var xScale = d3.scale.linear()
@@ -245,7 +181,7 @@ function update2() {
         .domain([0, d3.max(dataset2, function (d) {
             return d.n;
         })])
-        .range([2, 15]);
+        .range([0, 15]);
 
 
     //---------------------axes-----------------------------
@@ -261,24 +197,17 @@ function update2() {
         .call(xAxis)
         .attr("margin-top", 10);
 
-    /*
-
-     var yAxis = d3.svg.axis()
-     .scale(yScale)
-     .orient("left")
-     .ticks(d3.max(dataset2,function(d){return d.bacplus;}));
-
-     svg.append("g").attr("class","axis")
-     .attr("transform", "translate(" + (m-20) + ",0)")
-     .call(yAxis);
-
-     */
-
 
     //--------------------------------------Lignes-----------------------------
-    var dat = [2, 3, 4, 5];
-    //dat=d3.range(2,6);
-    var lines = svg.selectAll("line.h").data(dat);
+    //var dat = [ 1, 2, 3, 4, 5, 6, 7];
+    var min = d3.min(dataset2, function (d) {
+        return d.bacplus;
+    });
+    var max = d3.max(dataset2, function (d) {
+        return d.bacplus;
+    });
+    var dat = d3.range(min, max + 1);
+    var lines = svg.selectAll("line.h").data(dat)
     lines.enter()
         .append("line")
         .attr("class", 'h')
@@ -291,7 +220,8 @@ function update2() {
             return yScale(d);
         })
         .attr("stroke-width", 1)
-        .attr("stroke", "#eee");
+        .attr("stroke", "black")
+        .attr("stroke-opacity", 0.4);
 
     //Légende
     var legend = svg.selectAll("text.t")
@@ -311,95 +241,116 @@ function update2() {
         })
         .attr("font-family", "sans-serif")
         .attr("font-size", "4px")
-        .attr("fill", "darkblue");
+        .attr("fill", "dark")
+        .attr("opacity", 0.6);
 
 
     d3.selectAll("g.groups,circle").remove();
 
-    if($('input[name=options]:checked').attr('id')=="option1"){
-
-
-        /*var fpays = $("input[type=checkbox]:checked").each(
-            function() {
-                //return ($(this).attr("id"));
-            }
-        ); */
-
+    if ($('input[name=options]:checked').attr('id') == "option1") {
 
 
         var groups = svg.selectAll("g.groups").data(dataset2)
                 .enter()
 
-            .append('g').attr('class', 'groups')
-            .attr("transform", function (d) {
-                return 'translate(' + xScale(d.naissance) + ',' + yScale(d.bacplus) + ')';
-            })
-            .selectAll("circle.peep")
-            .data(function (d) {
+                .append('g').attr('class', 'groups')
+                .attr("transform", function (d) {
+                    return 'translate(' + xScale(d.naissance) + ',' + yScale(d.bacplus) + ')';
+                })
+                .selectAll("circle.peep")
+                .data(function (d) {
                     //console.log(d)
                     return d.values
-            })
+                })
                 .enter()
 
                 .append('circle').attr('class', 'peep')
-                .filter(function(d){
-                    var checks=$('#pays input.checkbox:checked')
-                    for(var i=0;i<checks.length;i++){
+                .filter(function (d) {
+                    var checks = $('#pays input.checkbox:checked');
+                    for (var i = 0; i < checks.length; i++) {
                         //var id=checks[i].id;
-                        if(checks[i].id== d.pays)return true;
+                        if (checks[i].id == d.pays)return true;
                     }
+                })
 
-                    return false;
+                .filter(function (d) {
+                    var checks = $('#sexe input.checkbox:checked');
+                    for (var i = 0; i < checks.length; i++) {
+                        //var id=checks[i].id;
+                        if (checks[i].id == 'Homme' && d.sexe == 1)return true;
+                        if (checks[i].id == 'Femme' && d.sexe == 2)return true;
+                        if (checks[i].id == 'Autre' && d.sexe == 0)return true;
+                    }
+                })
+
+
+                .filter(function (d) {
+                    var checks = $('#finance input.checkbox:checked');
+                    for (var i = 0; i < checks.length; i++) {
+                        if (checks[i].id == d.finance)return true;
+                    }
 
                 })
 
-                .filter(function(d){
-                    var checks=$('#finance input.checkbox:checked');
-                    for (var i=0;i<checks.length;i++){
-                        if (checks[i].id== d.finance)return true;
+                .filter(function (d) {
+                    var checks = $('#statut input.checkbox:checked');
+                    for (var i = 0; i < checks.length; i++) {
+                        //var id=checks[i].id;
+                        if (checks[i].id == d.statut)return true;
                     }
-
                 })
+
 
                 .attr("cx", 0).attr("cy", function (d, i) {
-                return i * -7;
-            }).attr("r", 3)
-            .attr('stroke-width', 0)
-            .attr("fill", function (d) {
-                return color(d.pays);
-            })
-            .on('mouseover', function (d) {
+                    return i * -7;
+                }).attr("r", 3)
+                .attr('stroke-width', 0)
+                .attr("fill", function (d) {
+                    if ($('input[name=Option]:checked').attr('id') == "option3") {
+                        return color(d.pays);
+                    }
+                    if ($('input[name=Option]:checked').attr('id') == "option4") {
+                        return color(d.finance);
+                    }
+                    if ($('input[name=Option]:checked').attr('id') == "option5") {
+                        return color(d.sexe);
+                    }
+                    if ($('input[name=Option]:checked').attr('id') == "option6") {
+                        return color(d.statut);
+                    }
+
+                })
+                /*.on('mouseover', function (d) {
                     d3.select(this).attr('stroke-width', 3)
                         .attr("fill", function (d) {
                             return color(d.pays);
                         })
-                        .attr("stroke",function (d) {
+                        .attr("stroke", function (d) {
                             return color(d.pays);
                         })
                 })
-            .on('mouseout', function (d) {
-                d3.select(this).attr('stroke-width', 0);
-                d3.select(this).attr("fill", function (d) {
-                    return color(d.pays);
+                .on('mouseout', function (d) {
+                    d3.select(this).attr('stroke-width', 0);
+                    d3.select(this).attr("fill", function (d) {
+                        return color(d.pays);
+                    })
+                })*/
+                .append("svg:title")
+                .text(function (d, i) {
+                    return "Année de naissance : " + d.naissance + "\nNiveau d'études : bac+" + d.bacplus + "\nPays d'origine : " + d.pays;
                 })
-            })
-            .append("svg:title")
-            .text(function (d, i) {
-                return "Année de naissance : " + d.naissance + "\nNiveau d'études : bac+" + d.bacplus + "\nPays d'origine : " + d.pays;
-            })
 
 
             ;
 
 
-
-    }else{
+    } else {
 
 
         var circles = svg.selectAll("circle.blue").data(dataset2);
-        circles.enter()
-            .append("circle")
-            .attr("class",'blue')
+
+        circles.enter().append("circle").attr("class", 'blue')
+
             .attr('stroke-width', 0)
             .attr("cx", function (d) {
                 return xScale(d.naissance);
@@ -408,9 +359,41 @@ function update2() {
                 return yScale(d.bacplus);
             })
             .attr("r", function (d) {
-                return zScale(d.n);
+
+                //console.log('d.values',d.values)
+
+                //filtrer ici
+
+
+                var v = d.values.filter(function (d) {
+                    var checks = $('#finance input.checkbox:checked');
+                    for (var i = 0; i < checks.length; i++) {
+                        if (checks[i].id == d.finance)return true;
+                    }
+                    return false;
+                })
+
+
+                    .filter(function (d) {
+                        var checks = $('#pays input.checkbox:checked');
+                        for (var i = 0; i < checks.length; i++) {
+                            //var id=checks[i].id;
+                            if (checks[i].id == d.pays)return true;
+                        }
+                    })
+
+                    .filter(function (d) {
+                        var checks = $('#statut input.checkbox:checked');
+                        for (var i = 0; i < checks.length; i++) {
+                            //var id=checks[i].id;
+                            if (checks[i].id == d.statut)return true;
+                        }
+                    })
+                return zScale(v.length);
             })
-            .attr("class", "cercle")
+
+            .attr("fill", "#c00")
+            .attr('opacity', 0.6)
             .on('mouseover', function (d) {
                 //var a = xScale(d[0]);
                 //var b = yScale(d[1]);
@@ -432,40 +415,18 @@ function update2() {
     }
 
 
-
-
 }
 
-$('input').change(function(){update2()});
 
-
-
-    /*svg.selectAll("button")
-        .data(dat).enter()
-        .append("button")
-        .
-*/
-
-
-    /*circles.transition(100)
-     .attr("r",function(d){return zScale(1);});
-
-
-
-     circles.exit()
-     .attr("r",function(d){return 0;})
-     .remove();
-
-     */
-
-
-    //-----------------------------------FILTRES-------------------------------------------------------------
-
-    /*var checkboxFilter = svg.selectAll("checkbox").on("change",function(){
-        var type = this.checked ? 'inline', 'none';
-
-        svg.selectAll(.peep)
-        .filter(function(d))
+$(document).ready(function(){
+    $(window).resize(function(){
+        var hauteur = $(window).height();
+        var largeur = $(window).width();
+        console.log("hauteur="+hauteur,"largeur="+largeur);
+        var h=300;
+        var l=$('.container').width()-240;
+        svg.attr("width",l);
+        svg.attr("height",h);
+        update2();
     })
-
-    */
+})
