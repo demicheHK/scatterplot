5aacc76c2efddc7b4c74dfa63b6036aacffab479
camilledemichel <c.camilledemichel@gmail.com>
2015-08-14 17:40:04 +0800

    update4

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,7 @@
  * Created by tristanmaurel on 24/07/15.
  */
 
+
 var svg = d3.select("#graph").append("svg");
 
 var dataset2 = [];
@@ -17,8 +18,6 @@ var dataset = [];
 //d3.csv("scripts/qd.csv")
 d3.csv("scripts/questionnaire.csv")
     .row(function(d, i) {
-        //console.log(d);
-        console.log(d['Quelle est votre année de naissance ?']);
         if(isNaN(bac(d["Quel est votre niveau de formation académique ?"])))return;
         if(d['Quelle est votre année de naissance ?']<1895)return;
         var obj={
@@ -35,7 +34,6 @@ d3.csv("scripts/questionnaire.csv")
         return obj;
     })
     .get(function(error, rows) {
-        //console.log(rows);
         dataset=rows;
         dataset2 = d3.nest()
             .key(function(d){return d.uid;})
@@ -49,7 +47,6 @@ d3.csv("scripts/questionnaire.csv")
         affichePays();
         d3.selectAll('input.checkbox').attr('checked','true');
         $('input').change(function(){update2()});
-
         update2();
     });
 
@@ -62,7 +59,6 @@ var color=d3.scale.category10();
 
 
 
-console.log('dataset',dataset);
 
 function bac (s) {
     s = s.replace("BAC + ","");
@@ -73,18 +69,6 @@ function bac (s) {
 function removeone(){
     dataset.pop();
 }
-
-
-
-
-//hauteur et largeur du scatter plot à revoir
-var h=300;
-var l=$('.container').width();
-svg.attr("width",l);
-svg.attr("height",h);
-//marge aux extrémités
-var m = 80;
-
 
 
 
@@ -104,11 +88,9 @@ function listePays(a) {
 
 function affichePays() {
     var listedePays = listePays(dataset);
-    console.log(listedePays);
     var blocHTML = "";
 
    for (var k in listedePays) {
-       console.log(k,listedePays[k]);
        blocHTML = blocHTML + "<label for=\"" + k + "\" style='font-weight: normal'><input type=checkbox class=checkbox id=\"" + k + "\"> &nbsp " + k + " <span style='color: " + color(k) + "'><strong> " + listedePays[k] + " </strong></span></label><br>";
    };
 
@@ -117,38 +99,18 @@ function affichePays() {
 
 
 
-function listeFinance(a) {
-    var counts = {};
-    a.forEach(function (d) {
-        if (!counts[d.finance]) {
-            counts[d.finance] = 0;
-        }
-        counts[d.finance]++;
-    });
-    return counts;
-};
-
-function afficheFinance() {
-    var listedeFinance = listeFinance(dataset);
-    console.log(listedeFinance);
-    var blocHTML = "";
-
-    for (var f in listedeFinance) {
-        console.log(f,listedeFinance[f]);
-        blocHTML = blocHTML + "<p><label for=" + f + " style='font-weight: normal'><input type=checkbox class=checkbox id=" + f + "> &nbsp " + f + " <span style='color: " + color(f) + "'><strong> " + listedeFinance[f] + " </strong></span></label></p>";
-    };
-
-    $("#finance").html(blocHTML);
-};
-
-
-
-
-
-
 // ---------------------------------------------------UPDATE2-----------------------------------------------------------
 function update2() {
-    console.log('update2()');
+
+    svg.selectAll('*').remove();
+
+    var h=300;
+    var l=$('.container').width();
+    svg.attr("width",l);
+    svg.attr("height",h);
+//marge aux extrémités
+    var m = 80;
+
 
     d3.selectAll(".axis").remove();
 
@@ -200,56 +162,57 @@ function update2() {
 
     //--------------------------------------Lignes-----------------------------
     //var dat = [ 1, 2, 3, 4, 5, 6, 7];
-    var min = d3.min(dataset2, function (d) {
-        return d.bacplus;
-    });
-    var max = d3.max(dataset2, function (d) {
-        return d.bacplus;
-    });
-    var dat = d3.range(min, max + 1);
-    var lines = svg.selectAll("line.h").data(dat)
-    lines.enter()
-        .append("line")
-        .attr("class", 'h')
-        .attr("x1", 60)
-        .attr("y1", function (d) {
-            return yScale(d);
-        })
-        .attr("x2", l)
-        .attr("y2", function (d) {
-            return yScale(d);
-        })
-        .attr("stroke-width", 1)
-        .attr("stroke", "black")
-        .attr("stroke-opacity", 0.4);
+        var min = d3.min(dataset2, function (d) {
+            return d.bacplus;
+        });
+        var max = d3.max(dataset2, function (d) {
+            return d.bacplus;
+        });
+        var dat = d3.range(min, max + 1);
+        var lines = svg.selectAll("line.h").data(dat)
+        lines.enter()
+            .append("line")
+            .attr("class", 'h')
+            .attr("x1", 60)
+            .attr("y1", function (d) {
+                return yScale(d);
+            })
+            .attr("x2", l)
+            .attr("y2", function (d) {
+                return yScale(d);
+            })
+            .attr("stroke-width", 1)
+            .attr("stroke", "black")
+            .attr("stroke-opacity", 0.4);
 
-    //Légende
-    var legend = svg.selectAll("text.t")
-        .data(dat)
-        .enter()
-        .append("text")
-        .attr("class", "t")
-        .attr("x", (5))
-        .attr("y", function (d) {
-            return yScale(d);
-        })
-        .attr("text-anchor", "left")
-        .style("font-size", "12px")
-        .style("text-decoration", "none")
-        .text(function (d) {
-            return "Bac +" + d;
-        })
-        .attr("font-family", "sans-serif")
-        .attr("font-size", "4px")
-        .attr("fill", "dark")
-        .attr("opacity", 0.6);
-
+        //Légende
+        var legend = svg.selectAll("text.t")
+            .data(dat)
+            .enter()
+            .append("text")
+            .attr("class", "t")
+            .attr("x", (5))
+            .attr("y", function (d) {
+                return yScale(d);
+            })
+            .attr("text-anchor", "left")
+            .style("font-size", "12px")
+            .style("text-decoration", "none")
+            .text(function (d) {
+                return "Bac +" + d;
+            })
+            .attr("font-family", "sans-serif")
+            .attr("font-size", "4px")
+            .attr("fill", "dark")
+            .attr("opacity", 0.6);
 
     d3.selectAll("g.groups,circle").remove();
 
     if ($('input[name=options]:checked').attr('id') == "option1") {
 
+        $("#propriétés").addClass("hide");
         $("#color").removeClass("hide");
+
 
         var groups = svg.selectAll("g.groups").data(dataset2)
                 .enter()
@@ -260,7 +223,6 @@ function update2() {
                 })
                 .selectAll("circle.peep")
                 .data(function (d) {
-                    //console.log(d)
                     return d.values
                 })
                 .enter()
@@ -307,16 +269,16 @@ function update2() {
                 }).attr("r", 3)
                 .attr('stroke-width', 0)
                 .attr("fill", function (d) {
-                    if ($('input[name=Option]:checked').attr('id') == "option3") {
+                    if ($('input[name=colorbtn]:checked').attr('id') == "option4") {
                         return color(d.pays);
                     }
-                    if ($('input[name=Option]:checked').attr('id') == "option4") {
+                    if ($('input[name=colorbtn]:checked').attr('id') == "option5") {
                         return color(d.finance);
                     }
-                    if ($('input[name=Option]:checked').attr('id') == "option5") {
+                    if ($('input[name=colorbtn]:checked').attr('id') == "option6") {
                         return color(d.sexe);
                     }
-                    if ($('input[name=Option]:checked').attr('id') == "option6") {
+                    if ($('input[name=colorbtn]:checked').attr('id') == "option7") {
                         return color(d.statut);
                     }
 
@@ -345,8 +307,9 @@ function update2() {
             ;
 
 
-    } else {
+    } if ($('input[name=options]:checked').attr('id') == "option2") {
         $("#color").addClass("hide");
+        $("#propriétés").addClass("hide");
 
         var circles = svg.selectAll("circle.blue").data(dataset2);
 
@@ -361,7 +324,6 @@ function update2() {
             })
             .attr("r", function (d) {
 
-                //console.log('d.values',d.values)
 
                 //filtrer ici
 
@@ -423,10 +385,129 @@ function update2() {
         circles.exit().remove();
 
 
+    }if ($('input[name=options]:checked').attr('id') == "option3") {
+        $("#color").addClass("hide");
+        $("#propriétés").removeClass("hide");
+        updateBubble();
+
     }
 
 
 }
+
+function updateBubble () {
+    console.log('updateBubble ()',dataset.length);
+    svg.selectAll('*').remove();
+
+    var diameter = svg.attr("height"),
+        format = d3.format(",d"),
+        color = d3.scale.category20();
+
+
+    var bubble = d3.layout.pack()
+        .sort(null)
+        .size([diameter, diameter])
+        .padding(1.5);
+
+    //d3.json("scripts/package.json", function(error, root) {
+    //    if (error) throw error;
+
+    var root={};
+
+    var data = d3.nest()
+        .key(function(d){
+            var id=$('input[name=Option]:checked').attr('id');
+            console.log('input checked id=',id);
+            if (id == "property1") {
+                return d.pays;
+            }
+
+            if (id == "property2") {
+                return d.formation;
+            }
+
+            if (id == "property3") {
+                if (d.sexe==0){
+                    return "Homme";
+                }
+                if (d.sexe==1){
+                    return "Femme";
+                }
+                if (d.sexe==2){
+                    return "Autre";
+                }
+            }
+
+            if (id == "property4") {
+                return d.statut;
+            }
+
+            console.log("truc qui merde :",id);
+
+            ;})
+        .entries(dataset);
+
+    // modifier 'root' pour lui ajouter la propriete size
+    // parcourir le tableau root; et ajouter la propriete size a chaque element du tableau
+
+    for (var i=0; i<data.length; i++){
+        data[i].size=data[i].values.length;
+        data[i].name=data[i].key;
+        delete data[i].values;
+        delete data[i].key;
+        console.log('size', data[i].size);
+    };
+
+    console.log('data',data);
+    if (data.length==0){
+        console.log("c la merde");
+        return;
+    }
+    root = {"children": data};
+
+    var datahuit=classes(root);
+
+        var node = svg.selectAll(".node")
+            .data(bubble.nodes(datahuit)
+                .filter(function(d) { return !d.children; }))
+            .enter().append("g")
+            .attr("class", "node")
+            .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+
+
+        node.append("title")
+            .text(function(d) { return d.className + ": " + format(d.value); });
+
+        node.append("circle")
+            .attr("r", function(d) { return d.r; })
+            .style("fill", function(d) { return color(d.className); });
+
+        node.append("text")
+            .attr("dy", ".3em")
+            .style("text-anchor", "middle")
+            .text(function(d) { return d.className.substring(0, d.r); });
+    //});
+
+
+    // Returns a flattened hierarchy containing all leaf nodes under the root.
+    function classes(root) {
+        var classes = [];
+
+        function recurse(name, node) {
+            if (node.children) node.children.forEach(function(child) { recurse(node.name, child); });
+            else classes.push({packageName: name, className: node.name, value: node.size});
+        }
+
+        recurse(null, root);
+        return {children: classes};
+    }
+
+
+    //d3.select(self.frameElement).style("height", diameter + "px");
+
+}
+
+
 
 
 $(document).ready(function(){
